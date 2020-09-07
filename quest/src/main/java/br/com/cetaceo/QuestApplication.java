8294eb50972b71d69c3063b52968dcbf124efd17
thiag <thiag@10.0.0.105>
2020-09-07 10:12:38 -0300

    Configuração de um Interceptor para repassar o token no header das requisições feitas entre microserviços que utilizam o Feign.

--- quest/src/main/java/br/com/cetaceo/QuestApplication.java
+++ quest/src/main/java/br/com/cetaceo/QuestApplication.java
@@ -27,7 +27,8 @@ public class QuestApplication {
 	
 	/**
 	 * O Feign não consegue repassar os headers da requisição de origem para o microserviço de destino.
-	 * Nesse caso, quando a requisição chega em um endpoint protegido, é causada um erro de unauthorized (não autorizado).
+	 * Nesse caso, quando a requisição chega em um endpoint protegido, é causada um erro de unauthorized (não autorizado),
+	 * pois quando a requisição chega no outro microserviço o token não existe.
 	 * 
 	 * Para resolver esse problema, é criado um Interceptor faz esse trabalho, pois ele é executado antes de cada requisição
 	 * que utiliza o Feign.
