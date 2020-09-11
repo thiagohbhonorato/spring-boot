90857cf5cd6cca32c7910b92b20b0f7afd7a3a76
thiag <thiag@10.0.0.105>
2020-09-10 23:39:26 -0300

    Criação de um endpoint (/auth) no microserviço Quest que realiza a validação do usuário da aplicação e usuário final (pessoa). A configuração de acesso da aplicação está no application.properties.

--- quest/src/main/java/br/com/cetaceo/controller/AuthController.java
+++ quest/src/main/java/br/com/cetaceo/controller/AuthController.java
@@ -1,22 +1,30 @@
 package br.com.cetaceo.controller;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Base64;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import br.com.cetaceo.form.LoginForm;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+	
+	@Value("${security.oauth2.resource.oauth-token-uri}")
+	private String oauthTokenUri;
 	
 	@Value("${security.oauth2.client.client-id}")
 	private String clientId;
@@ -31,23 +39,25 @@ public class AuthController {
 	private String scope;
 	
 	@PostMapping
-	public Object auth(@RequestBody @Valid LoginForm login) {
+	public ResponseEntity<Object> auth(@RequestBody @Valid LoginForm login) {
 		
-		Map<String, String> params = new HashMap<String, String>();
+		String authorization = Base64.getEncoder().encodeToString( (clientId + ":" + secret).getBytes() );
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Authorization", "Basic " + authorization);
+		headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+		
+		MultiValueMap<String, Object> form = new LinkedMultiValueMap<>();
+		form.add("scope", scope);
+		form.add("grant_type", grantType);
+	    form.add("username", login.getUsername());
+	    form.add("password", login.getPassword());
 	    
-		params.put("grant_type", grantType);
-	    params.put("client_id", clientId);
-	    params.put("scope", scope);
+	    HttpEntity<MultiValueMap<String, Object>> entity = new HttpEntity<>(form, headers);
 	    
-	    params.put("username", login.getUsername());
-	    params.put("password", login.getPassword());
+		RestTemplate client = new RestTemplate();
+		ResponseEntity<Object> auth = client.postForEntity(oauthTokenUri, entity, Object.class);
 	    
-//	    Response response = RestAssured.given().auth().preemptive()
-//	      .basic(clientId, secret).and().with().params(params).when()
-//	      .post("http://localhost:8081/spring-security-oauth-server/oauth/token");
-//	    
-	    //return response.jsonPath().getString("access_token");
-	    
-	    return params;
+	    return auth;
 	}
 }
